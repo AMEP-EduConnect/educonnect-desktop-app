02269caaea842712e78f3ceb6342f6023e131a34
Gguardiola <gabriel.guardiola1999@gmail.com>
2024-03-02 20:34:19 +0100

    Initial environment and project configuration

--- AssemblyInfo.cpp
+++ AssemblyInfo.cpp
@@ -6,15 +6,15 @@ using namespace System::Runtime::CompilerServices;
 using namespace System::Runtime::InteropServices;
 using namespace System::Security::Permissions;
 
-[assembly:AssemblyTitleAttribute(L"CppCLRWinFormsProject")];
-[assembly:AssemblyDescriptionAttribute(L"")];
+[assembly:AssemblyTitleAttribute(L"EduConnect")];
+[assembly:AssemblyDescriptionAttribute(L"EduConnect - La teva plataforma per estudiar en grup")];
 [assembly:AssemblyConfigurationAttribute(L"")];
-[assembly:AssemblyCompanyAttribute(L"")];
-[assembly:AssemblyProductAttribute(L"CppCLRWinFormsProject")];
-[assembly:AssemblyCopyrightAttribute(L"Copyright (c)  2022")];
-[assembly:AssemblyTrademarkAttribute(L"")];
+[assembly:AssemblyCompanyAttribute(L"AMEP Corp.")];
+[assembly:AssemblyProductAttribute(L"EduConnect Desktop App")];
+[assembly:AssemblyCopyrightAttribute(L"Copyright (c)  2024 AMEP Corp.")];
+[assembly:AssemblyTrademarkAttribute(L"AMEP Corp.")];
 [assembly:AssemblyCultureAttribute(L"")];
 
-[assembly:AssemblyVersionAttribute(L"1.0.*")];
+[assembly:AssemblyVersionAttribute(L"0.0.0.0")];
 
 [assembly:ComVisible(false)];
