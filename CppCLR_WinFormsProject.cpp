cc838f43defc009d0b2598aa94f2500d829de566
Gguardiola <gabriel.guardiola1999@gmail.com>
2024-03-13 19:21:36 +0100

    test commit

--- CppCLR_WinFormsProject.cpp
+++ CppCLR_WinFormsProject.cpp
@@ -17,6 +17,6 @@ int main()
 {
   Application::EnableVisualStyles();
   Application::SetCompatibleTextRenderingDefault(false);
-  Application::Run(gcnew CppCLRWinFormsProject::MyForm());
+  Application::Run(gcnew CppCLRWinFormsProject::MainPage());
   return 0;
 }
