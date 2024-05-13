ada73b0a8f7c69a10bbea6fc5546b98fcc52643c
zitroupc <pau.ortiz.borras@estudiantat.upc.edu>
2024-05-13 14:15:38 +0200

    issue 32 fixed

--- GrupEstudi_EditarUI.h
+++ GrupEstudi_EditarUI.h
@@ -60,7 +60,8 @@ namespace CppCLRWinFormsProject {
 	private: GrupEstudiService^ grupEstudiService;
 
 	private: String^ nomActual;
-
+	private: System::Windows::Forms::Button^ noModificarButton;
+	private: bool noModifica = false;
 	private: String^ descripcioActual;
 
 #pragma region Windows Form Designer generated code
@@ -74,6 +75,7 @@ namespace CppCLRWinFormsProject {
 			this->NomActual_TextBox = (gcnew System::Windows::Forms::TextBox());
 			this->Nom_Label = (gcnew System::Windows::Forms::Label());
 			this->Editar_Panel = (gcnew System::Windows::Forms::Panel());
+			this->noModificarButton = (gcnew System::Windows::Forms::Button());
 			this->Edita_Button = (gcnew System::Windows::Forms::Button());
 			this->EditarDescripcio_TextBox = (gcnew System::Windows::Forms::TextBox());
 			this->Descripcio_Label = (gcnew System::Windows::Forms::Label());
@@ -92,9 +94,10 @@ namespace CppCLRWinFormsProject {
 			this->EditarGrupEstudi_Label->Font = (gcnew System::Drawing::Font(L"Inter", 20.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
 			this->EditarGrupEstudi_Label->ForeColor = System::Drawing::SystemColors::HotTrack;
-			this->EditarGrupEstudi_Label->Location = System::Drawing::Point(273, 34);
+			this->EditarGrupEstudi_Label->Location = System::Drawing::Point(374, 41);
+			this->EditarGrupEstudi_Label->Margin = System::Windows::Forms::Padding(4, 0, 4, 0);
 			this->EditarGrupEstudi_Label->Name = L"EditarGrupEstudi_Label";
-			this->EditarGrupEstudi_Label->Size = System::Drawing::Size(266, 33);
+			this->EditarGrupEstudi_Label->Size = System::Drawing::Size(336, 41);
 			this->EditarGrupEstudi_Label->TabIndex = 0;
 			this->EditarGrupEstudi_Label->Text = L"Edita Grup d\'Estudi";
 			this->EditarGrupEstudi_Label->Click += gcnew System::EventHandler(this, &GrupEstudi_EditarUI::EditarGrupEstudi_Label_Click);
@@ -103,9 +106,10 @@ namespace CppCLRWinFormsProject {
 			// 
 			this->NomActual_TextBox->Font = (gcnew System::Drawing::Font(L"Inter", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->NomActual_TextBox->Location = System::Drawing::Point(31, 46);
+			this->NomActual_TextBox->Location = System::Drawing::Point(41, 57);
+			this->NomActual_TextBox->Margin = System::Windows::Forms::Padding(4);
 			this->NomActual_TextBox->Name = L"NomActual_TextBox";
-			this->NomActual_TextBox->Size = System::Drawing::Size(392, 27);
+			this->NomActual_TextBox->Size = System::Drawing::Size(521, 32);
 			this->NomActual_TextBox->TabIndex = 2;
 			// 
 			// Nom_Label
@@ -114,9 +118,10 @@ namespace CppCLRWinFormsProject {
 			this->Nom_Label->Font = (gcnew System::Drawing::Font(L"Inter", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
 			this->Nom_Label->ForeColor = System::Drawing::SystemColors::HotTrack;
-			this->Nom_Label->Location = System::Drawing::Point(27, 24);
+			this->Nom_Label->Location = System::Drawing::Point(36, 30);
+			this->Nom_Label->Margin = System::Windows::Forms::Padding(4, 0, 4, 0);
 			this->Nom_Label->Name = L"Nom_Label";
-			this->Nom_Label->Size = System::Drawing::Size(113, 19);
+			this->Nom_Label->Size = System::Drawing::Size(138, 24);
 			this->Nom_Label->TabIndex = 3;
 			this->Nom_Label->Text = L"Nom del grup";
 			this->Nom_Label->Click += gcnew System::EventHandler(this, &GrupEstudi_EditarUI::Nom_Label_Click);
@@ -125,25 +130,46 @@ namespace CppCLRWinFormsProject {
 			// 
 			this->Editar_Panel->Anchor = System::Windows::Forms::AnchorStyles::None;
 			this->Editar_Panel->BackColor = System::Drawing::SystemColors::GradientInactiveCaption;
+			this->Editar_Panel->Controls->Add(this->noModificarButton);
 			this->Editar_Panel->Controls->Add(this->Edita_Button);
 			this->Editar_Panel->Controls->Add(this->EditarDescripcio_TextBox);
 			this->Editar_Panel->Controls->Add(this->Descripcio_Label);
 			this->Editar_Panel->Controls->Add(this->Nom_Label);
 			this->Editar_Panel->Controls->Add(this->NomActual_TextBox);
 			this->Editar_Panel->ForeColor = System::Drawing::SystemColors::HotTrack;
-			this->Editar_Panel->Location = System::Drawing::Point(179, 138);
+			this->Editar_Panel->Location = System::Drawing::Point(240, 170);
+			this->Editar_Panel->Margin = System::Windows::Forms::Padding(4);
 			this->Editar_Panel->Name = L"Editar_Panel";
-			this->Editar_Panel->Size = System::Drawing::Size(454, 292);
+			this->Editar_Panel->Size = System::Drawing::Size(605, 359);
 			this->Editar_Panel->TabIndex = 4;
+			// 
+			// noModificarButton
+			// 
+			this->noModificarButton->Anchor = System::Windows::Forms::AnchorStyles::None;
+			this->noModificarButton->BackColor = System::Drawing::Color::Transparent;
+			this->noModificarButton->Cursor = System::Windows::Forms::Cursors::Hand;
+			this->noModificarButton->Font = (gcnew System::Drawing::Font(L"Inter", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->noModificarButton->ForeColor = System::Drawing::Color::Red;
+			this->noModificarButton->Location = System::Drawing::Point(40, 292);
+			this->noModificarButton->Margin = System::Windows::Forms::Padding(4);
+			this->noModificarButton->Name = L"noModificarButton";
+			this->noModificarButton->Size = System::Drawing::Size(130, 43);
+			this->noModificarButton->TabIndex = 6;
+			this->noModificarButton->Text = L"Cancel·la";
+			this->noModificarButton->UseVisualStyleBackColor = true;
+			this->noModificarButton->Visible = false;
+			this->noModificarButton->Click += gcnew System::EventHandler(this, &GrupEstudi_EditarUI::noModificarButton_Click);
 			// 
 			// Edita_Button
 			// 
 			this->Edita_Button->Cursor = System::Windows::Forms::Cursors::Hand;
 			this->Edita_Button->Font = (gcnew System::Drawing::Font(L"Inter", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->Edita_Button->Location = System::Drawing::Point(324, 237);
+			this->Edita_Button->Location = System::Drawing::Point(432, 292);
+			this->Edita_Button->Margin = System::Windows::Forms::Padding(4);
 			this->Edita_Button->Name = L"Edita_Button";
-			this->Edita_Button->Size = System::Drawing::Size(99, 35);
+			this->Edita_Button->Size = System::Drawing::Size(132, 43);
 			this->Edita_Button->TabIndex = 7;
 			this->Edita_Button->Text = L"Modifica";
 			this->Edita_Button->UseVisualStyleBackColor = true;
@@ -154,10 +180,11 @@ namespace CppCLRWinFormsProject {
 			this->EditarDescripcio_TextBox->BorderStyle = System::Windows::Forms::BorderStyle::FixedSingle;
 			this->EditarDescripcio_TextBox->Font = (gcnew System::Drawing::Font(L"Inter", 11.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->EditarDescripcio_TextBox->Location = System::Drawing::Point(31, 120);
+			this->EditarDescripcio_TextBox->Location = System::Drawing::Point(41, 148);
+			this->EditarDescripcio_TextBox->Margin = System::Windows::Forms::Padding(4);
 			this->EditarDescripcio_TextBox->Multiline = true;
 			this->EditarDescripcio_TextBox->Name = L"EditarDescripcio_TextBox";
-			this->EditarDescripcio_TextBox->Size = System::Drawing::Size(392, 93);
+			this->EditarDescripcio_TextBox->Size = System::Drawing::Size(522, 114);
 			this->EditarDescripcio_TextBox->TabIndex = 5;
 			// 
 			// Descripcio_Label
@@ -166,11 +193,12 @@ namespace CppCLRWinFormsProject {
 			this->Descripcio_Label->Font = (gcnew System::Drawing::Font(L"Inter", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
 			this->Descripcio_Label->ForeColor = System::Drawing::SystemColors::HotTrack;
-			this->Descripcio_Label->Location = System::Drawing::Point(27, 98);
+			this->Descripcio_Label->Location = System::Drawing::Point(36, 121);
+			this->Descripcio_Label->Margin = System::Windows::Forms::Padding(4, 0, 4, 0);
 			this->Descripcio_Label->Name = L"Descripcio_Label";
-			this->Descripcio_Label->Size = System::Drawing::Size(203, 19);
+			this->Descripcio_Label->Size = System::Drawing::Size(193, 24);
 			this->Descripcio_Label->TabIndex = 4;
-			this->Descripcio_Label->Text = L"Descripci\u00F3 del grup";
+			this->Descripcio_Label->Text = L"Descripció del grup";
 			// 
 			// Cancelar_Button
 			// 
@@ -180,9 +208,10 @@ namespace CppCLRWinFormsProject {
 			this->Cancelar_Button->Font = (gcnew System::Drawing::Font(L"Inter", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
 			this->Cancelar_Button->ForeColor = System::Drawing::Color::Red;
-			this->Cancelar_Button->Location = System::Drawing::Point(687, 485);
+			this->Cancelar_Button->Location = System::Drawing::Point(917, 597);
+			this->Cancelar_Button->Margin = System::Windows::Forms::Padding(4);
 			this->Cancelar_Button->Name = L"Cancelar_Button";
-			this->Cancelar_Button->Size = System::Drawing::Size(89, 35);
+			this->Cancelar_Button->Size = System::Drawing::Size(119, 43);
 			this->Cancelar_Button->TabIndex = 6;
 			this->Cancelar_Button->Text = L"Tornar";
 			this->Cancelar_Button->UseVisualStyleBackColor = false;
@@ -190,9 +219,10 @@ namespace CppCLRWinFormsProject {
 			// 
 			// Background_PictureBox
 			// 
-			this->Background_PictureBox->Location = System::Drawing::Point(-5, 555);
+			this->Background_PictureBox->Location = System::Drawing::Point(-7, 683);
+			this->Background_PictureBox->Margin = System::Windows::Forms::Padding(4);
 			this->Background_PictureBox->Name = L"Background_PictureBox";
-			this->Background_PictureBox->Size = System::Drawing::Size(928, 265);
+			this->Background_PictureBox->Size = System::Drawing::Size(1237, 326);
 			this->Background_PictureBox->SizeMode = System::Windows::Forms::PictureBoxSizeMode::StretchImage;
 			this->Background_PictureBox->TabIndex = 15;
 			this->Background_PictureBox->TabStop = false;
@@ -211,23 +241,25 @@ namespace CppCLRWinFormsProject {
 			this->tableLayoutPanel1->Controls->Add(this->Editar_Panel, 1, 1);
 			this->tableLayoutPanel1->Dock = System::Windows::Forms::DockStyle::Fill;
 			this->tableLayoutPanel1->Location = System::Drawing::Point(0, 0);
+			this->tableLayoutPanel1->Margin = System::Windows::Forms::Padding(4);
 			this->tableLayoutPanel1->Name = L"tableLayoutPanel1";
 			this->tableLayoutPanel1->RowCount = 3;
 			this->tableLayoutPanel1->RowStyles->Add((gcnew System::Windows::Forms::RowStyle(System::Windows::Forms::SizeType::Percent, 18.80819F)));
 			this->tableLayoutPanel1->RowStyles->Add((gcnew System::Windows::Forms::RowStyle(System::Windows::Forms::SizeType::Percent, 68.34264F)));
 			this->tableLayoutPanel1->RowStyles->Add((gcnew System::Windows::Forms::RowStyle(System::Windows::Forms::SizeType::Percent, 12.7045F)));
-			this->tableLayoutPanel1->Size = System::Drawing::Size(814, 537);
+			this->tableLayoutPanel1->Size = System::Drawing::Size(1085, 661);
 			this->tableLayoutPanel1->TabIndex = 16;
 			this->tableLayoutPanel1->Paint += gcnew System::Windows::Forms::PaintEventHandler(this, &GrupEstudi_EditarUI::tableLayoutPanel1_Paint);
 			// 
 			// GrupEstudi_EditarUI
 			// 
-			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(814, 537);
+			this->ClientSize = System::Drawing::Size(1085, 661);
 			this->Controls->Add(this->tableLayoutPanel1);
 			this->Controls->Add(this->Background_PictureBox);
 			this->FormBorderStyle = System::Windows::Forms::FormBorderStyle::None;
+			this->Margin = System::Windows::Forms::Padding(4);
 			this->Name = L"GrupEstudi_EditarUI";
 			this->StartPosition = System::Windows::Forms::FormStartPosition::CenterParent;
 			this->Text = L"EduConnect";
@@ -243,6 +275,7 @@ namespace CppCLRWinFormsProject {
 #pragma endregion
 private: System::Void CancelarButton_Click(System::Object^ sender, System::EventArgs^ e);
 private: System::Void testEdita_Button(System::Object^ sender, System::EventArgs^ e);
+private: System::Void noModificarButton_Click(System::Object^ sender, System::EventArgs^ e);
 private: System::Void GrupEstudi_EditarUI_Load(System::Object^ sender, System::EventArgs^ e) {
 }
 private: System::Void EduConnect_Label_Click(System::Object^ sender, System::EventArgs^ e) {
